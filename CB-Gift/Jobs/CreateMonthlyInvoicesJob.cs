9eb81947e2987845b63930c7f71dcc87351ebcd8
Nguyen Quang Huy K17 HL <quanghuytn2022@gmail.com>
2025-12-22 01:57:32 +0700

    update ngày tạo hóa đơn

--- CB-Gift/Jobs/CreateMonthlyInvoicesJob.cs
+++ CB-Gift/Jobs/CreateMonthlyInvoicesJob.cs
@@ -21,8 +21,8 @@ namespace CB_Gift.Jobs
             // 1. Xác định tháng/năm mục tiêu
             // Vì Job chạy vào ngày 10, chúng ta cần lập hóa đơn cho tháng trước (Current Date - 1 month)
             var targetDate = DateTime.Today.AddMonths(-1);
-            var year = targetDate.Year;
-            var month = targetDate.Month;
+            var year = targetDate.Year; // year của năm hiện tại
+            var month = targetDate.Month; // tháng trước 
 
             // 2. Gọi phương thức service
             // "system" là User ID đại diện cho việc Job tự động tạo
